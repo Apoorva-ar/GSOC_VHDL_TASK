5a0e6fb26228b6a6d3cd095ae782295ee7ff116c
apoorvaarora <apoorvaarora79@gmail.com>
2020-04-17 19:14:35 +0200

    Add files via upload
    build file

---
+++ build.tcl
@@ -0,0 +1,6 @@
+
+prj_project new -name "uart_spi" -impl "impl1" -dev LCMXO2-1200HC-5TG144C -synthesis "lse"
+prj_src add "C:/Users/rahul/Desktop/gsoc_task/GSOC_VHDL_TASK-master/bridge.vhd" "C:/Users/rahul/Desktop/gsoc_task/GSOC_VHDL_TASK-master/sclk_gen.vhd" "C:/Users/rahul/Desktop/gsoc_task/GSOC_VHDL_TASK-master/spi_data_path.vhd" "C:/Users/rahul/Desktop/gsoc_task/GSOC_VHDL_TASK-master/spi_master.vhd" "C:/Users/rahul/Desktop/gsoc_task/GSOC_VHDL_TASK-master/uart.vhd"
+prj_project save
+prj_run Synthesis -impl impl1
+prj_project close
